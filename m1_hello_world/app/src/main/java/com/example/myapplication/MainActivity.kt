91f912ebb7c4d747c4bd77acda4d8bf532a643c3
Anvyar <anvyar.narbikov@ya.ru>
2022-01-30 16:30:54 +0300

    Corrected task lesson 1

--- m1_hello_world/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ m1_hello_world/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,86 +2,65 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import androidx.core.content.res.ResourcesCompat
 import com.example.myapplication.databinding.ActivityMainBinding
+import kotlin.math.absoluteValue
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        setOnClickListeners(binding)
+    }
+}
+    private fun setOnClickListeners (binding: ActivityMainBinding) {
         var counter = 0
-
-        fun defineView() {
-            when (counter) {
-                0 -> {
-                    binding.centralTextView.text = resources.getString(
-                        R.string.centralTextViewDefault
-                    )
-                    binding.centralTextView.setTextColor(
-                        ResourcesCompat.getColor(resources, R.color.green, null)
-                    )
-                    binding.counterTextView.text = counter.toString()
-                    binding.decreasePassengerButton.setBackgroundColor(
-                        ResourcesCompat.getColor(resources, R.color.lightGrey, null)
-                    )
-                    binding.increasePassengerButton.setBackgroundColor(
-                        ResourcesCompat.getColor(resources, R.color.green, null)
-                    )
-                    binding.resetButton.visibility = View.INVISIBLE
-                }
-                in 1..49 -> {
-                    binding.centralTextView.text = "Seats left: ${50-counter}"
-                    binding.centralTextView.setTextColor(
-                        ResourcesCompat.getColor(
-                            resources, R.color.purple_700, null
-                        )
-                    )
-                    binding.counterTextView.text = counter.toString()
-                    binding.decreasePassengerButton.setBackgroundColor(
-                        ResourcesCompat.getColor(resources, R.color.green, null)
-                    )
-                    binding.increasePassengerButton.setBackgroundColor(
-                        ResourcesCompat.getColor(resources, R.color.green, null)
-                    )
-                    binding.resetButton.visibility = View.INVISIBLE
-                }
-                50 -> {
-                    binding.centralTextView.text =
-                        resources.getString(R.string.centralTextViewExceeds)
-                    binding.centralTextView.setTextColor(
-                        ResourcesCompat.getColor(resources, R.color.red, null)
-                    )
-                    binding.counterTextView.text = counter.toString()
-                    binding.decreasePassengerButton.setBackgroundColor(
-                        ResourcesCompat.getColor(resources, R.color.green, null)
-                    )
-                    binding.increasePassengerButton.setBackgroundColor(
-                        ResourcesCompat.getColor(resources, R.color.lightGrey, null)
-                    )
-                    binding.resetButton.visibility = View.VISIBLE
-                }
-            }
-        }
         binding.decreasePassengerButton.setOnClickListener {
             when (counter) {
-                0 -> defineView()
+                0 -> defineView(binding,counter)
                 in 1..50 -> {
                     counter--
-                    defineView()
+                    defineView(binding,counter)
                 }
             }
         }
         binding.increasePassengerButton.setOnClickListener {
             if (counter in 0..49) {
                 counter++
-                defineView()
+                defineView(binding,counter)
             }
         }
         binding.resetButton.setOnClickListener {
             counter = 0
-            defineView()
+            defineView(binding,counter)
+        }
+
+    }
+    private fun defineView(binding: ActivityMainBinding,counter:Int) {
+        when (counter) {
+            0 -> {
+                binding.centralTextView.text = binding.centralTextView.context.getString(R.string.centralTextViewDefault)
+                binding.centralTextView.setTextColor(binding.centralTextView.context.getColor(R.color.green))
+                binding.counterTextView.text = counter.toString()
+                binding.decreasePassengerButton.setBackgroundColor(binding.decreasePassengerButton.context.getColor(R.color.lightGrey))
+                binding.increasePassengerButton.setBackgroundColor(binding.increasePassengerButton.context.getColor(R.color.green.absoluteValue))
+                binding.resetButton.visibility = View.INVISIBLE
+            }
+            in 1..49 -> {
+                binding.centralTextView.text = binding.centralTextView.context.getString(R.string.seats_left_string,(50 - counter).toString())
+                binding.centralTextView.setTextColor(binding.centralTextView.context.getColor(R.color.purple_700))
+                binding.counterTextView.text = counter.toString()
+                binding.decreasePassengerButton.setBackgroundColor(binding.decreasePassengerButton.context.getColor(R.color.green))
+                binding.increasePassengerButton.setBackgroundColor(binding.increasePassengerButton.context.getColor(R.color.green))
+                binding.resetButton.visibility = View.INVISIBLE
+            }
+            50 -> {
+                binding.centralTextView.text = binding.centralTextView.context.getText(R.string.centralTextViewExceeds)
+                binding.centralTextView.setTextColor(binding.centralTextView.context.getColor(R.color.red))
+                binding.counterTextView.text = counter.toString()
+                binding.decreasePassengerButton.setBackgroundColor(binding.decreasePassengerButton.context.getColor(R.color.green))
+                binding.increasePassengerButton.setBackgroundColor(binding.increasePassengerButton.context.getColor(R.color.lightGrey))
+                binding.resetButton.visibility = View.VISIBLE
+            }
         }
     }
-}
