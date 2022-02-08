2e080b7cdbc7e01be9995f149d0ada959dfdcde1
Anvyar <anvyar.narbikov@ya.ru>
2022-02-08 21:49:30 +0300

    Finished task 3

--- m3_components/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ m3_components/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.myapplication
 
 import android.os.Bundle
 import android.widget.SeekBar
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.appcompat.app.AppCompatDelegate.MODE_NIGHT_NO
@@ -17,7 +18,6 @@ class MainActivity : AppCompatActivity() {
         dayNightMode(binding)
         seekBar(binding)
         startButton(binding)
-
     }
 }
 
@@ -34,6 +34,7 @@ fun seekBar(binding: ActivityMainBinding) {
         override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
             binding.counterTextView.text = p1.toString()
             binding.progressBar.max = p1
+
         }
 
         override fun onStartTrackingTouch(p0: SeekBar?) {}
@@ -42,20 +43,37 @@ fun seekBar(binding: ActivityMainBinding) {
 }
 
 fun startButton(binding: ActivityMainBinding) {
-    val scope = CoroutineScope(Job() + Dispatchers.Main)
     binding.startButton.setOnClickListener {
-        val secLeft = binding.progressBar.max
-        if (secLeft > 0) {
+        val scope = CoroutineScope(Job() + Dispatchers.Main)
+        if (binding.progressBar.max > 0) {
             scope.launch {
                 binding.seekBar2.isEnabled = false
-                binding.progressBar.progress = secLeft
-                for (i in 0 until binding.progressBar.max) {
+                binding.progressBar.progress = binding.progressBar.max
+                binding.startButton.text =
+                    binding.startButton.context.getString(R.string.stop_button)
+                binding.startButton.setBackgroundColor(binding.startButton.context.getColor(R.color.teal_200))
+                while (binding.progressBar.progress > 0) {
+                    binding.startButton.setOnClickListener {
+                        toDefault(binding)
+                    }
                     delay(1000)
                     binding.progressBar.progress--
-                    binding.counterTextView.text = "test"
+                    binding.counterTextView.text = (binding.progressBar.progress).toString()
+
                 }
-                binding.seekBar2.isEnabled = true
+                toDefault(binding)
+//                Toast.makeText("Test toast", Toast.LENGTH_LONG).show()
             }
         }
     }
+}
+
+fun toDefault(binding: ActivityMainBinding) {
+    binding.seekBar2.isEnabled = true
+    binding.seekBar2.progress = 0
+    binding.startButton.text = binding.startButton.context.getString(R.string.start_button)
+    binding.startButton.setBackgroundColor(binding.startButton.context.getColor(R.color.purple_500))
+    binding.progressBar.max = 0
+    binding.progressBar.progress = 0
+    startButton(binding)
 }
